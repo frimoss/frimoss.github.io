53fd79151713f60d4f2ee24b1e24ee1975241305
frimoss <78547341+frimoss@users.noreply.github.com>
2021-02-10 00:11:54 +0300

    Add files via upload

---
+++ 3/assets/js/main2.js
@@ -0,0 +1,25 @@
+/* Мобильное меню */
+$('.menu__burger').click(function() {
+    $('.menu__mobile .menu').toggle(500);
+    $(this).toggleClass('close');
+});
+
+/* Кнопки */
+$(function(){
+    var link1 = $('.btn1');
+    link1.click(function() {
+        $('.btn2').removeClass('active');
+        $('.main__russia').hide();
+        $('.btn1').addClass('active');
+        $('.main__abroad').show();
+    })
+})
+$(function(){
+    var link2 = $('.btn2');
+    link2.click(function() {
+        $('.btn1').removeClass('active');
+        $('.main__abroad').hide();
+        $('.btn2').addClass('active');
+        $('.main__russia').show();
+    })
+})
